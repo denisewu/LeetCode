eebcb1ffbe59de8a5c3cc309ae8688f88f47838f
Rong Wu <denisewu.ict@gmail.com>
2015-03-04 19:10:27 +0800

    Update LargestRectangleinHistogram.cpp

--- LargestRectangleinHistogram.cpp
+++ LargestRectangleinHistogram.cpp
@@ -1,3 +1,11 @@
+
+/*
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+For example,
+Given height = [2,1,5,6,2,3],
+return 10.
+*/
+
 int largestRectangleArea(vector<int> &height) 
 { 
 	stack<int> s;
