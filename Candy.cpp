0994b78e3034061e33b7c3c450795922c0779797
Rong Wu <denisewu.ict@gmail.com>
2015-03-04 18:51:04 +0800

    Update Candy.cpp

--- Candy.cpp
+++ Candy.cpp
@@ -1,22 +1,42 @@
-int candy(vector<int> &ratings) {
-	int sum = 0;
-	vector<int> candies = vector<int>(ratings.size());
-	for(int i = 0; i < ratings.size(); i++)
-	{
-		if(i == 0 || ratings[i] <= ratings[i - 1])
-			candies[i] = 1;
-		else if(ratings[i] > ratings[i - 1])
-			candies[i] = candies[i - 1] + 1;
+/*
+There are N children standing in a line. Each child is assigned a rating value.
 
-	}
-	for(int i = ratings.size() - 2; i >= 0; i--)
-	{
-		if(ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
-			candies[i] = candies[i + 1] + 1;
-	}
-	for(int i = 0; i < ratings.size(); i++)
-		sum += candies[i];
-	return sum;
+You are giving candies to these children subjected to the following requirements:
 
-}
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+*/
+
+
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        int sum = 0;
+        vector<int> candies(ratings.size());
+        for(int i = 0 ; i < ratings.size(); i++)
+        {
+            if(i == 0)
+                candies[i] = 1;
+            else if(ratings[i] > ratings[i - 1])
+            {
+                candies[i] = candies[i - 1] + 1;
+            }
+            else
+                candies[i] = 1;
+        }
+        
+        for(int i = ratings.size() - 2; i >= 0; i--)
+        {
+            if(ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
+            {
+                candies[i] = candies[i + 1] + 1;
+            }
+            sum += candies[i + 1];
+        }
+        
+        sum += candies[0];
+        return sum;
+    }
+};
 
