bfbe3a3d803682fee8353949035a0f9123d83469
Rong Wu <denisewu.ict@gmail.com>
2015-03-06 20:02:03 +0800

    Update LetterCombinationOfPhoneNumber.cpp

--- LetterCombinationOfPhoneNumber.cpp
+++ LetterCombinationOfPhoneNumber.cpp
@@ -9,7 +9,7 @@ class Solution {
     vector<string> dict = 
         { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 public:
- ic:
+
     vector<string> letterCombinationsIter(string digits)
     {
        vector<string> ret;
@@ -36,36 +36,35 @@ public:
        return ret;
     }
   
-   vector<string> letterCombinationsRecursive(string digits) {
-        vector<string> ret;
-        if(digits.length() == 0)
-        {
-            ret.push_back("");
-            return ret;
-        }
-      
-        vector<string> strs = letterCombinations(digits.substr(0, digits.length() - 1));
-        char lastChar = digits[digits.length() - 1];
-        string lastMapping = dict[lastChar - '0' - 2];
-        for(int i = 0; i < lastMapping.length(); i++)
-        {
-          
-                for(int j = 0; j < strs.size(); j++)
-                {
-                    string s = strs[j] + lastMapping[i];
-                    ret.push_back(s);
-                }
-
-            
-        }
-        return ret;
  
+    vector<string> letterCombinationsAux(string digits) {
+        vector<string> ret;
+        if(digits.empty())
+            return ret;
+            
+        ret = letterCombinationsAux(digits.substr(0, digits.length() - 1));
+        if(ret.empty())
+            ret.push_back("");
+            
+        string mapped = dict[digits[digits.length() - 1] - '2'];
+        int len = ret.size();
+        for(int i = 0; i < len; i++)
+        {
+            string prefix = ret[i];
+            for(int j = 0; j < mapped.length(); j++)
+            {
+                string t = prefix;
+                t.append(1, mapped[j]);
+                ret.push_back(t);
+             }
+         }
+         ret.erase(ret.begin(), ret.begin() + len);
+         return ret;
     }
+    
     vector<string> letterCombinations(string digits)
     {
        // return letterCombinationsIter(digits);
-       return letterCombinationRecursive(digits);
+       return letterCombinationsAux(digits);
     }
-    
-   
 }
