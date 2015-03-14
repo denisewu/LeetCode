fafdc80d79d56af8ce7adda1f61bc1833385eafa
Rong Wu <denisewu.ict@gmail.com>
2015-03-14 18:32:51 +0800

    Create RegularExpressionMatching.cpp

---
+++ RegularExpressionMatching.cpp
@@ -0,0 +1,47 @@
+/*
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+*/
+
+
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if(*p == '\0')
+            return *s == '\0';
+        
+        if(*(p + 1) != '*')
+        {
+            if(*s != '\0' && (*s == *p || *p == '.'))
+                return isMatch(s + 1, p + 1);
+            else
+                return false;
+        }
+        else
+        {
+            while(*s != '\0' && (*s == *p || *p == '.'))
+            {
+                if(isMatch(s, p + 2))
+                    return true;
+                s++;
+            }
+            return isMatch(s, p + 2);
+        }
+    }
+};
